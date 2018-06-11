78fe424e98ea2cdcec0e6e829ff3d4ca22cdd1d4
jeerapote <jeerapote4@hotmail.com>
2018-06-11 14:14:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,17 +16,17 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 		
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
                         if($text=="สวัสดี"){
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
